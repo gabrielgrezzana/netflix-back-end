9af332f885b8e21f24db604549515a6caae3dc5e
gabrielgrezzana <106358234+gabrielgrezzana@users.noreply.github.com>
2023-02-19 20:28:36 -0300

    atualizacao app.service port

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'servidor rodando em http://localhost:3000';
+    return `servidor rodando em ${process.env.PORT}`;
   }
 }
